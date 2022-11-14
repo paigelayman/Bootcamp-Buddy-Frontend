6fad71f83b867061a922c637383ac0d71b848c86
Bret Borden <bordenb21@gmail.com>
2022-11-14 14:06:56 -0500

    added classes to register.js and initiated css for register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,87 +1,91 @@
-import { useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useState } from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 const Register = () => {
   // Variables
   const [formValues, setFormValues] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    confirmPassword: ""
-  });
-  const [isShowPassword, toggleIsShowPassword] = useState(false);
-  let navigate = useNavigate();
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+  const [isShowPassword, toggleIsShowPassword] = useState(false)
+  let navigate = useNavigate()
 
   // Functions
   const handlePasswordToggle = (e) => {
     if (e.target.checked) {
-      toggleIsShowPassword(true);
-      setFormValues({ ...formValues, confirmPassword: "" });
+      toggleIsShowPassword(true)
+      setFormValues({ ...formValues, confirmPassword: '' })
     } else {
-      toggleIsShowPassword(false);
+      toggleIsShowPassword(false)
     }
-  };
+  }
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     //do function await check email, if failed provide user
     //do function await register, if failed, provide feedback
     //do await login function for user automatically to get the token session
     setFormValues({
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-      confirmPassword: ""
-    });
-    navigate("/");
-  };
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    })
+    navigate('/')
+  }
 
   const handleChange = (e) => {
-    setFormValues({ ...formValues, [e.target.name]: e.target.value });
-  };
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
 
   // Renders
-  let passwordMatchRender = <div></div>;
+  let passwordMatchRender = <div></div>
   if (formValues.password && formValues.confirmPassword) {
     if (formValues.password !== formValues.confirmPassword) {
-      passwordMatchRender = <div>Password did not match!</div>;
+      passwordMatchRender = <div>Password did not match!</div>
     }
   }
 
   let passwordFieldRender = (
     <div>
-      <label>Password:</label>{" "}
-      <input
-        onChange={handleChange}
-        value={formValues.password}
-        name="password"
-        type="password"
-        required
-      />
-      <input
-        onChange={handlePasswordToggle}
-        value={isShowPassword}
-        name="isShowPassword"
-        type="checkbox"
-      />
-      <label>Show Password</label>
+      <div className="register-label">
+        <label>Password:</label>{' '}
+        <input
+          onChange={handleChange}
+          value={formValues.password}
+          name="password"
+          type="password"
+          required
+        />
+        <input
+          onChange={handlePasswordToggle}
+          value={isShowPassword}
+          name="isShowPassword"
+          type="checkbox"
+        />
+        <label>Show Password</label>
+      </div>
       <br />
-      <label>Confirm Password:</label>{" "}
-      <input
-        onChange={handleChange}
-        value={formValues.confirmPassword}
-        name="confirmPassword"
-        type="password"
-        required
-      />{" "}
+      <div className="register-label">
+        <label>Confirm Password:</label>{' '}
+        <input
+          onChange={handleChange}
+          value={formValues.confirmPassword}
+          name="confirmPassword"
+          type="password"
+          required
+        />{' '}
+      </div>
       {passwordMatchRender} <br />
     </div>
-  );
+  )
   if (isShowPassword) {
     passwordFieldRender = (
       <div>
-        <label>Password:</label>{" "}
+        <label>Password:</label>{' '}
         <input
           onChange={handleChange}
           value={formValues.password}
@@ -98,10 +102,10 @@ const Register = () => {
         <label>Show Password</label>
         <br />
       </div>
-    );
+    )
   }
 
-  let registerButtonRender = <button disabled>Register</button>;
+  let registerButtonRender = <button disabled>Register</button>
   if (
     formValues.firstName &&
     formValues.lastName &&
@@ -109,56 +113,62 @@ const Register = () => {
     formValues.password
   ) {
     if (isShowPassword) {
-      registerButtonRender = <button>Register</button>;
+      registerButtonRender = <button>Register</button>
     } else if (formValues.password === formValues.confirmPassword) {
-      registerButtonRender = <button>Register</button>;
+      registerButtonRender = <button>Register</button>
     }
   }
 
   let registerFormRender = (
     <div>
       <form onSubmit={handleSubmit}>
-        <label>First Name:</label>{" "}
-        <input
-          onChange={handleChange}
-          value={formValues.firstName}
-          name="firstName"
-          type="text"
-          placeholder="John"
-          required
-        />{" "}
+        <div className="register-label">
+          <label>First Name:</label>{' '}
+          <input
+            onChange={handleChange}
+            value={formValues.firstName}
+            name="firstName"
+            type="text"
+            placeholder="John"
+            required
+          />{' '}
+        </div>
         <br />
-        <label>Last Name:</label>{" "}
-        <input
-          onChange={handleChange}
-          value={formValues.lastName}
-          name="lastName"
-          type="text"
-          placeholder="Doe"
-          required
-        />{" "}
+        <div className="register-label">
+          <label>Last Name:</label>{' '}
+          <input
+            onChange={handleChange}
+            value={formValues.lastName}
+            name="lastName"
+            type="text"
+            placeholder="Doe"
+            required
+          />{' '}
+        </div>
         <br />
-        <label>Email:</label>{" "}
-        <input
-          onChange={handleChange}
-          value={formValues.email}
-          name="email"
-          type="email"
-          placeholder="john.doe@email.com"
-          required
-        />
+        <div className="register-label">
+          <label>Email:</label>{' '}
+          <input
+            onChange={handleChange}
+            value={formValues.email}
+            name="email"
+            type="email"
+            placeholder="john.doe@email.com"
+            required
+          />
+        </div>
         <br />
         {passwordFieldRender}
         {registerButtonRender}
       </form>
     </div>
-  );
+  )
   let toRender = (
-    <div>
+    <div className="register-container">
       <div>This is register page</div>
       {registerFormRender}
     </div>
-  );
-  return toRender;
-};
-export default Register;
+  )
+  return toRender
+}
+export default Register
