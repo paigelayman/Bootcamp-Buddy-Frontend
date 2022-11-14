c8501d39e03332d49d46a9b41f0d26bcda1f1d30
LaShelle Rauseo <lrauseo3@gmail.com>
2022-11-14 08:17:14 -0600

    Initial formatting cdd on Home

--- src/components/BootcampCard.jsx
+++ src/components/BootcampCard.jsx
@@ -1,7 +1,7 @@
 const BootcampCard = ({bootcamp}) => {
     return (
         <div className="grid col-3">
-            <div>{bootcamp.name}</div>
+            <div className="bootcamp-name">{bootcamp.name}</div>
             <div>{bootcamp.shortDescription}</div>
             <button>Join</button>
         </div>
