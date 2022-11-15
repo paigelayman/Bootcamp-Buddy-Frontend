a8b743bbb57ec93a36ed09ec36ba9103ef96ce5e
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-11-15 13:16:17 -0600

    Create initial form

---
+++ src/components/CreateReview.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const CreateReview = (props) => {
+    const [review, setReview] = useState("")
+    let handleChange = (e) => {
+        setReview(e.target.value)
+    }
+    let handleSubmit = () => {
+
+    }
+    return (
+        <div className="">
+            <label className="" htmlFor="comment">Comment</label>
+            <textarea
+              id="comment"
+              cols="30"
+              rows="10"
+              onChange={handleChange}
+              value={review}
+            ></textarea>
+            <button onClick={handleSubmit} type="submit" className="">
+              Submit
+            </button>
+          </div>
+    )
+}
+
+export default CreateReview
